8c2e38084245e616622562816857dff4c4c16f7d
adbeje <adria.venturaherce@gmail.com>
2020-05-19 12:51:43 -0700

    HomePage acabada

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <item v-for="contribution in contributions" :key="contribution.id" :contribution="contribution"></item>
+  </div>
+</template>
+
+<script>
+import Item from '@/views/Item.vue'
+import axios from "axios";
+export default {
+  name: 'home',
+  components: {
+    'item': Item
+  },
+  data: function() {
+    return {
+      err: "",
+      contributions: []
+    };
+  },
+
+  created: function() {
+    axios
+      .get("https://salty-inlet-98667.herokuapp.com/api/contribucions.json")
+      .then(result => {
+        this.contributions = result.data;
+        console.log(this.$store.state.articles);
+      })
+      .catch(err => {
+        this.err = err;
+      });
+  }
+};
+</script>
---
+++ src/views/New.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <item v-for="contribution in contriubtions" :key="contribution.id" :contribution="contribution"></item>
+  </div>
+</template>
+
+<script>
+import Item from '@/views/Item.vue'
+export default {
+  name: 'New',
+  components: {
+    'item': Item
+  },
+  data: function () {
+    return {
+        err: '',
+        contributions: this.$store.state.newContributions
+    }
+  },
+  created: function () {
+    if (this.$store.state.newContributions.length === 0) this.$store.dispatch('fetch_new_stories')
+  }
+}
+</script>
